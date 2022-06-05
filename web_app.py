4292c3c861d9b751620cc3e7edf315a8ee1a0a9d
Oussama Matar <oussamamatar@gmail.com>
2022-06-05 16:48:35 +0300

    Fixed web app image input

--- web_app.py
+++ web_app.py
@@ -8,21 +8,18 @@ mp_drawing_styles = mp.solutions.drawing_styles
 mp_pose = mp.solutions.pose
 
 
-def pose_mediapipe(image_path):
+def pose_mediapipe(image):
     BG_COLOR = (192, 192, 192)  # gray
     with mp_pose.Pose(
             static_image_mode=True,
             model_complexity=2,
             enable_segmentation=True,
             min_detection_confidence=0.5) as pose:
-        for idx, file in enumerate(IMAGE_FILES):
-            image = cv2.imread(file)
+        
             image_height, image_width, _ = image.shape
             # Convert the BGR image to RGB before processing.
             results = pose.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 
-            if not results.pose_landmarks:
-                continue
             print(
                 f'Nose coordinates: ('
                 f'{results.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE].x * image_width}, '
@@ -44,13 +41,13 @@ def pose_mediapipe(image_path):
                 results.pose_landmarks,
                 mp_pose.POSE_CONNECTIONS,
                 landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
-            print('Success!')
+            # print('Success!')
             # Plot pose world landmarks.
-            mp_drawing.plot_landmarks(
-                results.pose_world_landmarks, mp_pose.POSE_CONNECTIONS)
+            # mp_drawing.plot_landmarks(
+            #    results.pose_world_landmarks, mp_pose.POSE_CONNECTIONS)
             return annotated_image
 
 
-image_in = gr.inputs.Image(shape=(180, 180))
+image_in = gr.inputs.Image()
 app = gr.Interface(fn=pose_mediapipe, inputs=image_in, outputs='image')
-app.launch()
+app.launch(share=True)
